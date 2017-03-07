8570553c455fbcd759e89360756923f18f7b888d
Justin Chandra <jchan110@ucr.edu>
2017-03-06 18:31:52 -0800

    stuff

---
+++ iterator.cpp
@@ -0,0 +1,115 @@
+#include "iterator.h"
+
+//OPERATOR ITERATOR
+
+OperatorIterator::OperatorIterator(Base * ptr)
+{
+
+}
+
+void OperatorIterator::first()
+{
+
+}
+
+void OperatorIterator::next()
+{
+
+}
+
+bool OperatorIterator::is_done()
+{
+	return false;
+}
+
+Base * OperatorIterator::current()
+{
+	return NULL;
+}
+
+//END OPERATOR ITERATOR
+
+//UNARY ITERATOR
+UnaryIterator::UnaryIterator(Base * ptr)
+{
+
+}
+
+void UnaryIterator::first()
+{
+
+}
+
+void UnaryIterator::next()
+{
+
+}
+
+bool UnaryIterator::is_done()
+{
+	return false;
+}
+
+Base * UnaryIterator::current()
+{
+	return NULL;
+}
+
+
+//END UNARY ITERATOR
+
+//NULL ITERATOR
+
+NullIterator::NullIterator(Base * ptr)
+{
+
+}
+
+void NullIterator::first()
+{
+
+}
+
+void NullIterator::next()
+{
+
+}
+
+bool NullIterator::is_done()
+{
+	return false;
+}
+
+Base * NullIterator::current()
+{
+	return NULL;
+}
+
+//END NULL ITERATOR
+
+//PREORDER ITERATOR
+
+PreOrderIterator::PreOrderIterator(Base * ptr)
+{
+
+}
+
+void PreOrderIterator::first()
+{
+
+}
+
+void PreOrderIterator::next()
+{
+
+}
+
+bool PreOrderIterator::is_done()
+{
+	return false;
+}
+
+Base * PreOrderIterator::current()
+{
+	return NULL;
+}
