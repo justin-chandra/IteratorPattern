7b591f39084a4d4f37641ad06865941210dacb5e
Justin Chandra <jchan110@ucr.edu>
2017-03-06 18:21:35 -0800

    start stuff

---
+++ iterator.h
@@ -0,0 +1,26 @@
+#ifndef IT_H
+#define IT_H
+
+//also includes
+
+class Iterator
+{
+
+};
+
+class NullIterator: public Iterator
+{
+
+};
+
+class UnaryIterator: public Iterator
+{
+
+};
+
+class OperatorIterator: public Iterator
+{
+
+};
+
+#endif
