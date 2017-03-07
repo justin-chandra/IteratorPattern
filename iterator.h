27ca843dbfd7638a469b31728853380ce7ffba60
Audrey Der <ader003@ucr.edu>
2017-03-06 18:31:03 -0800

    iterator.h typed up givens

--- iterator.h
+++ iterator.h
@@ -5,22 +5,64 @@
 
 class Iterator
 {
+	protected:
+		Base * self_ptr;
+		Base * current_ptr;
 
+	public:
+		Iterator (Base *ptr)
+		{
+			this->self_ptr = ptr;
+		}
+
+		virtual void first() = 0;
+		virtual void next() = 0;
+		virtual bool is_done() = 0;
+		virtual Base* current() = 0;
 };
 
 class NullIterator: public Iterator
 {
-
+	public:
+		NullIterator(Base* ptr);
+		void first();
+		void next();
+		bool is_done();
+		Base* current();
 };
 
 class UnaryIterator: public Iterator
 {
-
+	public:
+		UnaryIterator(Base* ptr);
+		void first();
+		void next();
+		bool is_done();
+		Base* current();
 };
 
 class OperatorIterator: public Iterator
 {
-
+	
+	public:
+		OperatorIterator(Base* ptr);
+		void first();
+		void next();
+		bool is_done();
+		Base* current();
 };
+
+class PreOrderIterator: public Iterator
+{
+	protected:
+		stack<Iterator*> iterators;
+	
+	public:
+		PreOrderIterator(Base* ptr);
+		void first();
+		void next();
+		bool is_done();
+		Base* current();	
+}
 
 #endif
