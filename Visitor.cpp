a0e2192175104241d07f4afb4fbeba2255d4939f
Audrey Der <ader003@ucr.edu>
2017-03-13 18:29:42 -0700

    Visitor.cpp skeleton

--- Visitor.cpp
+++ Visitor.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
 #include "Visitor.h"
+
+void PrintVisitor::rootNode()
+{
+		
+}
+
+void PrintVisitor::sqrNode()
+{
+
+}
+
+void PrintVisitor::multNode()
+{
+
+}
+
+void PrintVisitor::subNode()
+{
+
+}
+
+void PrintVisitor::addNode()
+{
+
+}
+
+void PrintVisitor::opNode(Op * op)
+{
+
+}
+
+void PrintVisitor::execute()
+{
+
+}
